61ab9141fb48cc446c1b1be29681b48d8c3cc990
HALO\gangs <nprasad2077@gmail.com>
2025-01-17 20:09:30 -0600

    coolify config settings

--- NBAScoreboard/vite.config.js
+++ NBAScoreboard/vite.config.js
@@ -2,7 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  server: {
+    host: true,
+    port: 5173,
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+  },
 });
