87f23c7b86bbc334da4bc509e9c44117975b09f9
HALO\gangs <nprasad2077@gmail.com>
2025-01-22 18:00:51 -0600

    docker changes

--- nba_scoreboard_api/src/main.py
+++ nba_scoreboard_api/src/main.py
@@ -20,14 +20,17 @@ load_dotenv()
 
 from src.models import PlayerStatistics, PlayerData, TeamBoxScore, GameBoxScore
 
-# Add custom headers to the NBA API requests
+timeout = int(os.getenv('NBA_API_TIMEOUT', 120))
+retries = int(os.getenv('NBA_API_RETRIES', 3))
+
 custom_headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
     'Accept': 'application/json',
     'Accept-Language': 'en-US,en;q=0.9',
-    'Connection': 'keep-alive',
-    'Cache-Control': 'no-cache'
+    'Origin': 'https://stats.nba.com',
+    'Referer': 'https://stats.nba.com'
 }
+
 
 
 # Configure logging
